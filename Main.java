1cf9dcc533637688942ef5a5f78353d5874e7a5c
waseembaloch21 <waseemrauf672@gmail.com>
2025-08-10 19:42:42 +0500

    code updated

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
-public class Main{
-    public static void main(String[] args){
-        int age = 15;
-        if (age == 18 ) {
-             System.out.println("You are Adult");
-        }else{
-        System.out.println(" You are teenager");
+
+public class Main {
+
+    public static void main(String[] args) {
+        // int age = 15;
+        // if (age == 18 ) {
+        //      System.out.println("You are Adult");
+        // }else{
+        // System.out.println(" You are teenager");
+        // }
+        int grade = 85;
+        if (grade >= 80) {
+            System.out.println("A Grade");
+        } else if (grade >= 70) {
+            System.out.println(" B Grade");
+        } else {
+            System.out.println(" C Grade");
         }
     }
 }
