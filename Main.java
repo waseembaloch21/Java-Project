9a9d8ab6b101de25e59921b3fad1a67cb54ccc60
waseembaloch21 <waseemrauf672@gmail.com>
2025-08-10 19:34:01 +0500

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        int age = 15;
+        if (age == 18 ) {
+             System.out.println("You are Adult");
+        }else{
+        System.out.println(" You are teenager");
+        }
+    }
+}
